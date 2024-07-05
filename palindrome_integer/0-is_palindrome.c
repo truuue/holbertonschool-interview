6fe497c48fc46ae0f3345f5c5b145a0fc7da27cb
truuue <noahvernhet@gmail.com>
2024-07-05 09:48:12 +0200

    0. Palindrome unsigned integer

---
+++ palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,20 @@
+#include "palindrome.h"
+
+int is_palindrome(unsigned long n)
+{
+	unsigned long reversed = 0, remainder, original;
+
+	original = n;
+
+	while (n != 0)
+	{
+		remainder = n % 10;
+		reversed = reversed * 10 + remainder;
+		n /= 10;
+	}
+
+	if (original == reversed)
+		return (1);
+	else
+		return (0);
+}
