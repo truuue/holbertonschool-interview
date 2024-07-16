9924cc3e05dfc116abe4d9497eee3d42ec7fd9e8
truuue <noahvernhet@gmail.com>
2024-07-16 16:08:17 +0200

    Refactor UTF-8 validation function

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-""" UTF-8 Validation """
+"""
+UTF-8 validation module
+"""
+
 
 def validUTF8(data):
-    """ Determines if a given data set represents a valid UTF-8 encoding """
-    num_bytes = 0
+    """
+    function to check if data is valid utf-8
+    """
+    n_bytes = 0
 
     for num in data:
-        if num_bytes == 0:
-            mask = 1 << 7
-            while mask & num:
-                num_bytes += 1
-                mask >>= 1
+        bin_rep = format(num, '#010b')[-8:]
 
-            if num_bytes == 0 or num_bytes > 4:
+        if n_bytes == 0:
+            for bit in bin_rep:
+                if bit == '0':
+                    break
+                n_bytes += 1
+            if n_bytes == 0:
+                continue
+            if n_bytes == 1 or n_bytes > 4:
                 return False
-
-            if num_bytes > 1:
-                num_bytes -= 1
         else:
-            if (num >> 6) != 0b10:
+            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
                 return False
-
-            num_bytes -= 1
-
-    if num_bytes > 0:
-        return False
-
-    return True
+        n_bytes -= 1
+    return n_bytes == 0
