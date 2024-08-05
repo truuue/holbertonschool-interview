661e5204fb316948e5a86afb6cd1988fdda7d862
truuue <noahvernhet@gmail.com>
2024-08-05 11:34:38 +0200

    Add typedefs for avl_t and binary_tree_t

--- sorted_array_to_avl/binary_trees.h
+++ sorted_array_to_avl/binary_trees.h
@@ -22,6 +22,9 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
+
 void binary_tree_print(const binary_tree_t *);
 avl_t *sorted_array_to_avl(int *array, size_t size);
 
