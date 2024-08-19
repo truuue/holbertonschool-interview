8a3fe155b953f2671d983fda99a2d0de7be87ddd
truuue <noahvernhet@gmail.com>
2024-08-19 19:31:50 +0200

    Add script to fetch and display Star Wars characters from a specific movie

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require("request");
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error("Error:", error);
+    return;
+  }
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  characters.forEach(function (characterUrl) {
+    request(characterUrl, function (error, response, body) {
+      if (error) {
+        console.error("Error:", error);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
+});
