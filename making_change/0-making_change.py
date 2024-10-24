1cee5738bf802301dadaa76ef8e4acaf30b7e801
truuue <noahvernhet@gmail.com>
2024-10-24 13:55:40 +0200

    Changing approach

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -7,15 +7,21 @@ def makeChange(coins, total):
     Function that given a pile of coins of different values,
     determine the fewest number of coins needed to meet a given amount total
     """
-    if total < 1:
+    if total <= 0:
         return 0
-    change = 0
-    coins.sort(reverse=True)
-    for coin in coins:
-        temp_change = int(total / coin)
-        total -= (temp_change * coin)
-        change += temp_change
-        if total == 0:
-            return change
-    if total != 0:
+
+    # Initialiser un tableau pour stocker les résultats intermédiaires
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    # Remplir le tableau dp
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    # Si le montant total ne peut pas être atteint
+    if dp[total] == float('inf'):
         return -1
+
+    return dp[total]
