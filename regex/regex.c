3e05dbea35897e8c44f5095076ec5760b74d7519
truuue <noahvernhet@gmail.com>
2024-12-13 11:15:36 +0100

    Correct output error

--- regex/regex.c
+++ regex/regex.c
@@ -3,9 +3,9 @@
 static int match_star(char c, char const *pattern, char const *str);
 
 /**
- * match_here - Vérifie si pattern correspond à str à partir de la position actuelle
- * @str: Chaîne à analyser
- * @pattern: Expression régulière
+ * match_here - Verifie si pattern correspond à str a partir de la position actuelle
+ * @str: Chaine a analyser
+ * @pattern: Expression reguliere
  * Return: 1 si correspond, 0 sinon
  */
 static int match_here(char const *str, char const *pattern)
@@ -25,26 +25,31 @@ static int match_here(char const *str, char const *pattern)
 }
 
 /**
- * match_star - Gère le caractère * dans le pattern
- * @c: Caractère précédant *
- * @pattern: Reste du pattern après *
+ * match_star - Gere le caractere * dans le pattern
+ * @c: Caractere precede par *
+ * @pattern: Reste du pattern apres *
  * @str: Chaîne à analyser
  * Return: 1 si correspond, 0 sinon
  */
 static int match_star(char c, char const *pattern, char const *str)
 {
-	do {
+	if (match_here(str, pattern))
+		return 1;
+
+	while (*str && (c == '.' || c == *str))
+	{
+		str++;
 		if (match_here(str, pattern))
 			return 1;
-	} while (*str && (c == '.' || c == *str++) );
+	}
 
 	return 0;
 }
 
 /**
- * regex_match - Vérifie si un pattern correspond à une chaîne
- * @str: Chaîne à analyser
- * @pattern: Expression régulière
+ * regex_match - Verifie si un pattern correspond à une chaine
+ * @str: Chaine a analyser
+ * @pattern: Expression reguliere
  * Return: 1 si correspond, 0 sinon
  */
 int regex_match(char const *str, char const *pattern)
